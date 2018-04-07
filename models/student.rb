1346de13b360a5c87d35efacef396df968b70b35
Carol Greene <carolgreene3@gmail.com>
2018-04-07 21:01:53 +0000

    automatically backed up by learn

--- models/student.rb
+++ models/student.rb
@@ -1,15 +1,15 @@
-class Student 
-  attr_reader :name, :grade 
+class Student
+  attr_reader :name, :grade
 
   STUDENTS = []
 
   def initialize(params)
     @name = params[:name]
     @grade = params[:grade]
-    STUDENTS << self 
-  end 
+    STUDENTS << self
+  end
 
-  def self.all 
-    STUDENTS 
-  end 
+  def self.all
+    STUDENTS
+  end
 end    
